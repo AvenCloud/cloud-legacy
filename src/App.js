cef865ff013cbaa2c7629f007ef52fe819907f5a
Eric Vicenti <eric@aven.io>
2018-05-25 19:20:40 +0200

    darn gae port handling wtf

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text, Button } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+const Button = ({ title, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <Text>{title}</Text>
+  </TouchableOpacity>
+);
+
+const ScreenTitle = ({ children }) => <Text>{children}</Text>;
+const ScreenContainer = ({ children }) => <View>{children}</View>;
+
+const LoginScreen = ({}) => (
+  <ScreenContainer>
+    <ScreenTitle>Login</ScreenTitle>
+  </ScreenContainer>
+);
+const LogoutScreen = ({}) => (
+  <ScreenContainer>
+    <ScreenTitle>Logout</ScreenTitle>
+  </ScreenContainer>
+);
+const RegisterScreen = ({}) => (
+  <ScreenContainer>
+    <ScreenTitle>Register</ScreenTitle>
+  </ScreenContainer>
+);
+
+const screens = {
+  LoginScreen,
+  LogoutScreen,
+  RegisterScreen,
+};
+
+const LinkBar = ({ onLink }) => {
+  const links = Object.keys(screens).map(screenName => (
+    <View style={{ padding: 20 }}>
+      <Button
+        key={screenName}
+        onPress={() => {
+          console.log('dude');
+          onLink(screenName);
+        }}
+        title={screenName + ' ok'}
+      />
+    </View>
+  ));
+  return <View style={{ flexDirection: 'row', borderWidth: 2 }}>{links}</View>;
+};
 
 class App extends React.Component {
+  state = {
+    activeScreen: 'LoginScreen',
+  };
   render() {
     const { dispatch } = this.props;
+    const ActiveScreen = screens[this.state.activeScreen];
+    console.log('omg');
     return (
-      <ScrollView
-        style={{ flex: 1 }}
-        contentContainerStyle={{
-          justifyContent: 'center',
-          minHeight: '100%',
-        }}>
-        <Text>HJello</Text>
-      </ScrollView>
+      <View style={{ borderWidth: 12, borderColor: 'green' }}>
+        <Button title="ugh" onPress={() => {}} />
+        <a
+          onClick={() => {
+            console.log('shit');
+          }}>
+          shit
+        </a>
+        <ScrollView
+          style={{ flex: 1 }}
+          contentContainerStyle={{
+            justifyContent: 'center',
+            minHeight: '100%',
+          }}>
+          <LinkBar
+            onLink={activeScreen => {
+              debugger;
+              console.log('ok wtf', activeScreen);
+              this.setState({ activeScreen });
+            }}
+          />
+          {ActiveScreen && <ActiveScreen />}
+        </ScrollView>
+      </View>
     );
   }
 }
