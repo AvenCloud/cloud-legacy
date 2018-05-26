40a245b48cf050d370428d0128fbc3f8269e8b9d
Eric Vicenti <eric@aven.io>
2018-05-26 11:29:50 +0200

    ok thyen why pubsub

--- src/actions/SessionDestroy.js
+++ src/actions/SessionDestroy.js
@@ -14,13 +14,14 @@ export const schema = {
   additionalProperties: false,
 };
 
+console.log('launching pubsub ' + process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+const { publish } = pubsub.topic('session-destroy').publisher();
+
 export default async function SessionDestroy(action) {
   const message = Buffer.from(JSON.stringify({ session: action.session }));
 
-  pubsub
-    .topic('session-destroy')
-    .publisher()
-    .publish(message);
+  publish(message);
 
   return { ok: 'great' };
 }
--- src/pubsub.js
+++ src/pubsub.js
@@ -2,4 +2,6 @@ const PubSub = require(`@google-cloud/pubsub`);
 
 const pubsub = new PubSub();
 
+console.log('launching pubsub ' + process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 export default pubsub;
