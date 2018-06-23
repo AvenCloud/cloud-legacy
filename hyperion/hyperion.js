7d3f1abfbab66a1d9f2a55e4f7362cb02ae82ebe
Eric Vicenti <ericvicenti@users.noreply.github.com>
2018-06-23 05:26:06 -0700

    Update hyperion.js

--- hyperion/hyperion.js
+++ hyperion/hyperion.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hyperion Server');
+	res.send('Hyperion!');
 });
 
 app.get('/keys.public.txt', (req, res) => {
